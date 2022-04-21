3d591797638b484c5ff0f7974749f131c164d939
Mia Forsline <miaforsline@gmail.com>
2022-04-21 09:45:04 -0700

    editing cat function

--- functions/compute_cat.R
+++ functions/compute_cat.R
@@ -6,20 +6,20 @@
 #' @param h vegetation height (cm)
 #' @param v windspeed (cm/sec)
 #' @param kd default value of 0.7
-#' @param ko default value of 0.1
+#' @param k0 default value of 0.1
 #' @author Mia Forsline, Kristin Gill, Sydney Rilum
 #' @examples compute_cat(zm = 300, h = 100, v = 20, kd = 0.7,  ko = 0.1)
 #' @return atmospheric conductance (units?)
 
 
-compute_npv <- function(zm, h, v, kd, ko) {
-  if(zm - h < 200) stop("the height at which windspeed is measured in cm must be 200cm above the vegetation height")
+compute_cat <- function(zm, h, v, kd = 0.7, k0 = 0.1) {
+  if(zm - h < 200) stop("The height at which windspeed is measured in cm must be 200cm above the vegetation height")
   
+  zd = kd * h
   
+  z0 = k0 * h
   
-  cat = v / (6.25 * log( (zm - zd) / zo ))
+  cat = v / (6.25 * log( (zm - zd) / z0 )**2)
   
-  result = value / (1 + discount)**time
-  return(result)
 }
 
